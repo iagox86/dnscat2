f36edbd142e3bad62253d3af07d828c158c0505f
Ron <ron@skullsecurity.net>
2013-03-15 18:53:15 -0500

    Renamed 's' to 'pipe', which is a more logical name for it

--- server/dnscat2_server.rb
+++ server/dnscat2_server.rb
@@ -7,7 +7,7 @@ require 'session'
 # This class should be totally stateless, and rely on the Session class
 # for any long-term session storage
 class Dnscat2
-  def Dnscat2.handle_syn(s, packet, session)
+  def Dnscat2.handle_syn(pipe, packet, session)
     if(!session.syn_valid?())
       Log.log(session.id, "SYN invalid in this state")
       return nil
@@ -21,7 +21,7 @@ class Dnscat2
     return Packet.create_syn(session.id, session.my_seq, nil)
   end
 
-  def Dnscat2.handle_msg(s, packet, session)
+  def Dnscat2.handle_msg(pipe, packet, session)
     if(!session.msg_valid?())
       Log.log(session.id, "MSG invalid in this state")
       return nil
@@ -32,7 +32,7 @@ class Dnscat2
       Log.log(session.id, "Bad sequence number; expected 0x%04x, got 0x%04x [re-sending]" % [session.their_seq, packet.seq])
 
       # Re-send the last packet
-      old_data = session.read_outgoing(s.max_packet_size - Packet.msg_header_size)
+      old_data = session.read_outgoing(pipe.max_packet_size - Packet.msg_header_size)
       return Packet.create_msg(session.id, session.my_seq, session.their_seq, old_data)
     end
 
@@ -40,7 +40,7 @@ class Dnscat2
       Log.log(session.id, "Impossible ACK received: 0x%04x, current SEQ is 0x%04x [re-sending]" % [packet.ack, session.my_seq])
 
       # Re-send the last packet
-      old_data = session.read_outgoing(s.max_packet_size - Packet.msg_header_size)
+      old_data = session.read_outgoing(pipe.max_packet_size - Packet.msg_header_size)
       return Packet.create_msg(session.id, session.my_seq, session.their_seq, old_data)
     end
 
@@ -53,7 +53,7 @@ class Dnscat2
     # Increment the expected sequence number
     session.increment_their_seq(packet.data.length)
 
-    new_data = session.read_outgoing(s.max_packet_size - Packet.msg_header_size)
+    new_data = session.read_outgoing(pipe.max_packet_size - Packet.msg_header_size)
     Log.log(session.id, "Received MSG with #{packet.data.length} bytes; responding with our own message (#{new_data.length} bytes)")
     Log.log(session.id, ">> \"#{packet.data}\"")
     Log.log(session.id, "<< \"#{new_data}\"")
@@ -62,7 +62,7 @@ class Dnscat2
     return Packet.create_msg(session.id, session.my_seq, session.their_seq, new_data)
   end
 
-  def Dnscat2.handle_fin(s, packet, session)
+  def Dnscat2.handle_fin(pipe, packet, session)
     if(!session.fin_valid?())
       Log.log(session.id, "FIN invalid in this state")
       return nil
@@ -72,40 +72,40 @@ class Dnscat2
     return Packet.create_fin(session.id)
   end
 
-  def Dnscat2.go(s)
-    if(s.max_packet_size < 16)
+  def Dnscat2.go(pipe)
+    if(pipe.max_packet_size < 16)
       raise(Exception, "max_packet_size is too small")
     end
 
     session_id = nil
     begin
       loop do
-        packet = Packet.parse(s.recv())
+        packet = Packet.parse(pipe.recv())
         session = Session.find(packet.session_id)
 
         response = nil
         if(packet.type == Packet::MESSAGE_TYPE_SYN)
-          response = handle_syn(s, packet, session)
+          response = handle_syn(pipe, packet, session)
         elsif(packet.type == Packet::MESSAGE_TYPE_MSG)
-          response = handle_msg(s, packet, session)
+          response = handle_msg(pipe, packet, session)
         elsif(packet.type == Packet::MESSAGE_TYPE_FIN)
-          response = handle_fin(s, packet, session)
+          response = handle_fin(pipe, packet, session)
         else
           raise(IOError, "Unknown packet type: #{packet.type}")
         end
 
         if(response)
-          if(response.length > s.max_packet_size)
+          if(response.length > pipe.max_packet_size)
             raise(IOError, "Tried to send packet longer than max_packet_length")
           end
-          s.send(response)
+          pipe.send(response)
         end
       end
     rescue IOError => e
       if(!session_id.nil?)
         # Attempt to close the connection cleanly, ignoring errors
         begin
-          s.send(Packet.create_fin(session.id))
+          pipe.send(Packet.create_fin(session.id))
         rescue
         end
         Session.destroy(session_id)
@@ -115,6 +115,6 @@ class Dnscat2
       puts(e.backtrace)
     end
 
-    s.close()
+    pipe.close()
   end
 end
