2f6dba5c1251c946e7eb8f5e0b0c2833eb4b8c93
Ron <ron@skullsecurity.net>
2013-05-01 20:42:27 -0500

    Added log.c/log.h, accidentally missed them before:

---
+++ client/log.h
@@ -0,0 +1,28 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+typedef enum
+{
+  LOG_LEVEL_INFO    = 0,
+  LOG_LEVEL_WARNING = 1,
+  LOG_LEVEL_ERROR   = 2,
+  LOG_LEVEL_FATAL   = 3
+} log_level_t;
+
+#define LOG tolog
+
+#define LOG_INFO    log_info
+#define LOG_WARNING log_warning
+#define LOG_ERROR   log_error
+#define LOG_FATAL   log_fatal
+
+void log_to_file(char *filename, log_level_t min_level);
+void log_set_min_console_level(log_level_t level);
+void tolog(log_level_t level, char *format, ...);
+
+void log_info(char *format, ...);
+void log_warning(char *format, ...);
+void log_error(char *format, ...);
+void log_fatal(char *format, ...);
+
+#endif
