89371efb81adf5ac0a06ce8c9c7489af068ce8a2
Ron <ron@skullsecurity.net>
2013-07-04 08:25:52 -0700

    Added dnscat_exception.rb

---
+++ server/dnscat_exception.rb
@@ -0,0 +1,8 @@
+# dnscat_exception.rb
+# Created July 1, 2013 (Canada Day!)
+# By Ron Bowes
+#
+# Implements a simple exception class for DNSCat protocol errors.
+
+class DnscatException < Exception
+end
