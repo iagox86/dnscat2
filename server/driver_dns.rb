5a5421c572d16aa6ac50c32f3381d603dcc9596e
Ron <ron@skullsecurity.net>
2015-07-20 17:50:39 -0700

    Updated some documentation to fix issues/59

--- server/driver_dns.rb
+++ server/driver_dns.rb
@@ -94,9 +94,9 @@ class DriverDNS
   def initialize(host, port, domains)
     Log::PRINT(nil, "Starting Dnscat2 DNS server on #{host}:#{port} [domains = #{(domains.nil? || domains.length == 0) ? "n/a" : domains.join(", ")}]...")
     if(domains.nil? || domains.length == 0)
-      Log::PRINT(nil, "No domains were selected, which means this server will only respond to direct queries (using --host and --port on the client)")
+      Log::PRINT(nil, "No domains were selected, which means this server will only respond to direct queries (using \"--dns server=x.x.x.x,port=yyy\" on the client)")
     else
-      Log::PRINT(nil, "Will also accept direct queries (using --host and --port on the client)")
+      Log::PRINT(nil, "Will also accept direct queries (using \"--dns server=x.x.x.x,port=yyy\" on the client)")
     end
 
     @host        = host
