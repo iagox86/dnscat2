cf8c16b144eba71c3cd45feb70d7fe61271fd4db
Ron <ron@skullsecurity.net>
2015-09-18 16:26:08 -0700

    Added in a couple files that I'd previously forgotten

---
+++ server/drivers/driver_process.rb
@@ -0,0 +1,91 @@
+##
+# driver_console.rb
+# Created September 16, 2015
+# By Ron Bowes
+#
+# See: LICENSE.md
+#
+##
+
+require 'open3'
+
+class DriverProcess
+  def initialize(window, settings, process)
+    @window = window
+    @settings = settings
+    @outgoing = ""
+    @window.noinput = true
+
+    @window.puts("This isn't a console session!")
+    @window.puts()
+    @window.puts("The 'process' variable is set, which means that a specific")
+    @window.puts("process:")
+    @window.puts()
+    @window.puts(process)
+    @window.puts()
+    @window.puts("will be started. That process's i/o is bound to that dnscat2")
+    @window.puts("client, which means you can interact with the process via")
+    @window.puts("that client.")
+    @window.puts("")
+    @window.puts("Note that there is no access control, which means any client")
+    @window.puts("that connects to this server can also use this process; there")
+    @window.puts("are some security implications there!")
+    @window.puts()
+    @window.puts("To disable this, run 'set process=' in the main window.")
+    @window.puts()
+    @window.puts("To go back, type ctrl-z.")
+    @window.puts()
+
+    @done = false
+
+    # Do this in a thread, since read() blocks
+    @thread = Thread.new() do |thread|
+      # Put this in an error block, since Threads don't print errors when debug is off
+      begin
+        # popen2e combines stderr and stdout into a single pipe, which is handy
+        @window.puts("Starting process: #{process}")
+        Open3.popen2e(process) do |stdin, stdout, wait_thr|
+          # Save stdin so we can write to it when data comes
+          @process_stdin = stdin
+
+          # Read the output character by character.. I'm not sure if there's a better way, .gets() isn't
+          # binary friendly, and reading more than 1 byte means that buffering happens
+          while line = stdout.read(1)
+            @outgoing += line
+          end
+
+          # Get the exit status
+          exit_status = wait_thr.value
+
+          # TODO: Handle exit
+          if(!exit_status.success?)
+            @window.puts("Command exited with an error: #{process}")
+          else
+            @window.puts("Command exited successfully: #{process}")
+          end
+
+          @done = true
+
+        end
+      rescue Exception => e
+        $stdout.puts("ERROR: #{e}")
+      end
+    end
+  end
+
+  def feed(data)
+    if(@done)
+      return nil
+    end
+
+    @window.puts("[-->] #{data}")
+    @process_stdin.write(data)
+
+    out = @outgoing
+    @process_stdin.write(data)
+    @outgoing = ''
+
+    @window.puts("[<--] #{out}")
+    return out
+  end
+end
---
+++ server/libs/command_helpers.rb
@@ -0,0 +1,44 @@
+##
+# command_helpers.rb
+# Created September 16, 2015
+# By Ron Bowes
+#
+# See: LICENSE.md
+#
+# These are used both by controller_commands.rb and driver_command_commands.rb,
+# so instead of copying the code I put them together.
+#
+# It's unlikely that anybody else would have a use for this.
+##
+
+class CommandHelpers
+  # Displays the name and children of a window
+  def CommandHelpers._display_window(window, all, indent = 0)
+    if(!all && window.closed?())
+      return
+    end
+
+    @window.puts(('  ' * indent) + window.to_s())
+    window.children() do |c|
+      _display_window(c, all, indent + 1)
+    end
+  end
+
+  def CommandHelpers.display_windows(all)
+    _display_window(@window, all)
+  end
+
+  def CommandHelpers.wrap(s, width=72, indent=0)
+    return s.gsub(/(.{1,#{width}})(\s+|\Z)/, "#{" "*indent}\\1\n")
+  end
+
+  def CommandHelpers.format_field(s)
+    if(s.nil?)
+      return "(nil)"
+    elsif(s.is_a?(String))
+      return "'" + s + "'"
+    else
+      return s.to_s()
+    end
+  end
+end
