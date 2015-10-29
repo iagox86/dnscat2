78b5d3e69af76927eb957fc36600392722c9c0c0
Ron Bowes <rbowes@google.com>
2015-10-29 11:21:51 -0700

    Crypto/server: Moved the key values in Encryptor.rb into a hash. This is in preparation for re-keying

--- server/controller/encryptor.rb
+++ server/controller/encryptor.rb
@@ -16,8 +16,6 @@ require 'controller/encryptor_sas'
 require 'libs/dnscat_exception'
 
 class Encryptor
-  attr_reader :public_key_x, :public_key_y, :their_authenticator, :my_authenticator
-
   include EncryptorSAS
 
   ECDH_GROUP = ECDSA::Group::Nistp256
@@ -47,56 +45,80 @@ class Encryptor
   end
 
   def _create_key(key_name)
-    return SHA3::Digest::SHA256.digest(Encryptor.bignum_to_binary(@shared_secret) + key_name)
+    return SHA3::Digest::SHA256.digest(Encryptor.bignum_to_binary(@keys[:shared_secret]) + key_name)
   end
 
   def _create_authenticator(name, preshared_secret)
     return SHA3::Digest::SHA256.digest(name +
-      Encryptor.bignum_to_binary(@shared_secret) +
-      Encryptor.bignum_to_binary(@their_public_key.x) +
-      Encryptor.bignum_to_binary(@their_public_key.y) +
-      Encryptor.bignum_to_binary(@my_public_key.x) +
-      Encryptor.bignum_to_binary(@my_public_key.y) +
+      Encryptor.bignum_to_binary(@keys[:shared_secret]) +
+      Encryptor.bignum_to_binary(@keys[:their_public_key].x) +
+      Encryptor.bignum_to_binary(@keys[:their_public_key].y) +
+      Encryptor.bignum_to_binary(@keys[:my_public_key].x) +
+      Encryptor.bignum_to_binary(@keys[:my_public_key].y) +
       preshared_secret
     )
   end
 
-  def initialize(their_public_key_x, their_public_key_y, preshared_secret)
-    @my_nonce = -1
-    @their_nonce = -1
+  def initialize(preshared_secret)
+    @preshared_secret = preshared_secret
 
-    #@my_private_key = 1 + SecureRandom.random_number(ECDH_GROUP.order - 1)
-    @my_private_key = 43951174550322390566248264230503370167897136622816423934586799375127212096389
-    @my_public_key  = ECDH_GROUP.generator.multiply_by_scalar(@my_private_key)
-    @their_public_key = ECDSA::Point.new(ECDH_GROUP, their_public_key_x, their_public_key_y)
+    # Start with encryption turned off
+    @keys = {
+      :my_nonce            => -1,
+      :their_nonce         => -1,
+      :my_private_key      => nil,
+      :my_public_key       => nil,
+      :their_public_key    => nil,
+      :shared_secret       => nil,
+      :their_authenticator => nil,
+      :my_authenticator    => nil,
+      :their_write_key     => nil,
+      :their_mac_key       => nil,
+      :my_write_key        => nil,
+      :my_mac_key          => nil,
+    }
+    @old_keys = @keys.clone()
+  end
 
-    @shared_secret = @their_public_key.multiply_by_scalar(@my_private_key).x
+  def set_their_public_key(their_public_key_x, their_public_key_y)
+    @old_keys = @keys.clone()
 
-    @their_authenticator = _create_authenticator("client", preshared_secret)
-    @my_authenticator    = _create_authenticator("server", preshared_secret)
+    @keys = {
+      :my_nonce => -1,
+      :their_nonce => -1,
+    }
 
-    @their_write_key  = _create_key("client_write_key")
-    @their_mac_key    = _create_key("client_mac_key")
-    @my_write_key    = _create_key("server_write_key")
-    @my_mac_key      = _create_key("server_mac_key")
+    @keys[:my_private_key]      = 1 + SecureRandom.random_number(ECDH_GROUP.order - 1)
+    @keys[:my_public_key]       = ECDH_GROUP.generator.multiply_by_scalar(@keys[:my_private_key])
+    @keys[:their_public_key]    = ECDSA::Point.new(ECDH_GROUP, their_public_key_x, their_public_key_y)
+
+    @keys[:shared_secret]       = @keys[:their_public_key].multiply_by_scalar(@keys[:my_private_key]).x
+
+    @keys[:their_authenticator] = _create_authenticator("client", @preshared_secret)
+    @keys[:my_authenticator]    = _create_authenticator("server", @preshared_secret)
+
+    @keys[:their_write_key]     = _create_key("client_write_key")
+    @keys[:their_mac_key]       = _create_key("client_mac_key")
+    @keys[:my_write_key]        = _create_key("server_write_key")
+    @keys[:my_mac_key]          = _create_key("server_mac_key")
   end
 
   def to_s()
     out = []
-    out << "My private key:       #{Encryptor.bignum_to_text(@my_private_key)}"
-    out << "My public key [x]:    #{Encryptor.bignum_to_text(@my_public_key.x)}"
-    out << "My public key [y]:    #{Encryptor.bignum_to_text(@my_public_key.y)}"
-    out << "Their public key [x]: #{Encryptor.bignum_to_text(@their_public_key.x)}"
-    out << "Their public key [y]: #{Encryptor.bignum_to_text(@their_public_key.y)}"
-    out << "Shared secret:        #{Encryptor.bignum_to_text(@shared_secret)}"
+    out << "My private key:       #{Encryptor.bignum_to_text(@keys[:my_private_key])}"
+    out << "My public key [x]:    #{Encryptor.bignum_to_text(@keys[:my_public_key].x)}"
+    out << "My public key [y]:    #{Encryptor.bignum_to_text(@keys[:my_public_key].y)}"
+    out << "Their public key [x]: #{Encryptor.bignum_to_text(@keys[:their_public_key].x)}"
+    out << "Their public key [y]: #{Encryptor.bignum_to_text(@keys[:their_public_key].y)}"
+    out << "Shared secret:        #{Encryptor.bignum_to_text(@keys[:shared_secret])}"
     out << ""
-    out << "Their authenticator:  #{@their_authenticator.unpack("H*")}"
-    out << "My authenticator:     #{@my_authenticator.unpack("H*")}"
+    out << "Their authenticator:  #{@keys[:their_authenticator].unpack("H*")}"
+    out << "My authenticator:     #{@keys[:my_authenticator].unpack("H*")}"
     out << ""
-    out << "Their write key: #{@their_write_key.unpack("H*")}"
-    out << "Their mac key:   #{@their_mac_key.unpack("H*")}"
-    out << "My write key:    #{@my_write_key.unpack("H*")}"
-    out << "My mac key:      #{@my_mac_key.unpack("H*")}"
+    out << "Their write key: #{@keys[:their_write_key].unpack("H*")}"
+    out << "Their mac key:   #{@keys[:their_mac_key].unpack("H*")}"
+    out << "My write key:    #{@keys[:my_write_key].unpack("H*")}"
+    out << "My mac key:      #{@keys[:my_mac_key].unpack("H*")}"
     out << ""
     out << "SAS: #{get_sas()}"
 
@@ -104,36 +126,36 @@ class Encryptor
   end
 
   def my_public_key_x()
-    return @my_public_key.x
+    return @keys[:my_public_key].x
   end
 
   def my_public_key_x_s()
-    return Encryptor.bignum_to_binary(@my_public_key.x)
+    return Encryptor.bignum_to_binary(@keys[:my_public_key].x)
   end
 
   def my_public_key_y()
-    return @my_public_key.y
+    return @keys[:my_public_key].y
   end
 
   def my_public_key_y_s()
-    return Encryptor.bignum_to_binary(@my_public_key.y)
+    return Encryptor.bignum_to_binary(@keys[:my_public_key].y)
   end
 
   def my_nonce()
-    return @my_nonce += 1
+    return @keys[:my_nonce] += 1
   end
 
   def _is_their_nonce_valid?(their_nonce)
-    if(their_nonce < @their_nonce)
+    if(their_nonce < @keys[:their_nonce])
       return false
     end
 
-    @their_nonce = their_nonce
+    @keys[:their_nonce] = their_nonce
     return true
   end
 
   def decrypt_packet(data, options)
-    encrypted_packet = EncryptedPacket.parse(data, @their_mac_key, @their_write_key, options)
+    encrypted_packet = EncryptedPacket.parse(data, @keys[:their_mac_key], @keys[:their_write_key], options)
 
     if(!_is_their_nonce_valid?(encrypted_packet.nonce.unpack("n").pop))
       return nil
@@ -143,6 +165,14 @@ class Encryptor
   end
 
   def encrypt_packet(packet, options)
-    EncryptedPacket.new([my_nonce()].pack("n"), packet).to_bytes(@my_mac_key, @my_write_key)
+    EncryptedPacket.new([my_nonce()].pack("n"), packet).to_bytes(@keys[:my_mac_key], @keys[:my_write_key])
+  end
+
+  def their_authenticator()
+    return @keys[:their_authenticator]
+  end
+
+  def my_authenticator()
+    return @keys[:my_authenticator]
   end
 end
