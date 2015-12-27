2c48ad9d9584605a807df9cc0c3a3c5e8507218b
Ron <ron@skullsecurity.net>
2015-12-26 17:13:06 -0800

    Tunnels2: Hooked the 'listen' command back up, but I'm not super happy with it yet.

--- server/drivers/driver_command_tunnels.rb
+++ server/drivers/driver_command_tunnels.rb
@@ -10,7 +10,9 @@ require 'libs/command_helpers'
 require 'libs/socketer'
 
 class ViaSocket
-  @@sockets = {}
+  attr_reader :tunnel_id
+
+  @@via_sockets = {}
 
   def initialize(driver, host, port, callbacks)
     @tunnel_id = nil
@@ -37,7 +39,7 @@ class ViaSocket
       end
 
       # Create a socket pair so we can use this with Socketer
-      v_socket, s_socket = UNIXSocket.pair()
+      @v_socket, s_socket = UNIXSocket.pair()
 
       # Get the tunnel_id
       @tunnel_id = response.get(:tunnel_id)
@@ -46,12 +48,12 @@ class ViaSocket
       @session = Socketer::Session.new(s_socket, "#{host}:#{port} via tunnel #{@tunnel_id}", callbacks)
 
       # Save ourselves in the list of instances
-      @@sockets[@tunnel_id] = v_socket
+      @@via_sockets[@tunnel_id] = self
 
       # Start a receive thread for the socket
       @thread = Thread.new() do
         loop do
-          data = v_socket.recv(Socketer::BUFFER)
+          data = @v_socket.recv(Socketer::BUFFER)
 
           driver._send_request(CommandPacket.new({
             :is_request => true,
@@ -63,16 +65,9 @@ class ViaSocket
         end
       end
 
-
-#      @tunnels[tunnel_id] = {
-#        :thread  => thread,
-#        :socket  => v_socket,
-#      }
-#
-#      # Do a special callback
-#      if(callbacks[:on_via])
-#        callbacks[:on_via].call(tunnel_id)
-#      end
+      if(callbacks[:on_via])
+        callbacks[:on_via].call(@session, self)
+      end
 
       # We're only ready AFTER we are prepared to receive data - this may do a callback instantly
       @session.ready!()
@@ -98,27 +93,41 @@ class ViaSocket
     @thread.exit()
   end
 
-  def ViaSocket.handle_packet(packet)
-    tunnel_id = packet.get(:tunnel_id)
-    socket = @@sockets[tunnel_id]
-    if(socket.nil?)
+  def _sneak_in_data(data)
+    @v_socket.write(data)
+  end
+
+  def ViaSocket.get(driver, tunnel_id)
+    via_socket = @@via_sockets[tunnel_id]
+    if(via_socket.nil?)
       puts("ERROR: Couldn't find the socket for tunnel #{tunnel_id}")
 
-      _send_request(CommandPacket.new({
+      driver._send_request(CommandPacket.new({
         :is_request => true,
         :request_id => request_id(),
         :command_id => CommandPacket::TUNNEL_CLOSE,
         :tunnel_id  => tunnel_id,
         :reason     => "Unknown tunnel: %d" % tunnel_id
       }), nil)
+
+      return nil
     end
+
+    return via_socket
+  end
+
+  def ViaSocket.handle_packet(driver, packet)
+    tunnel_id = packet.get(:tunnel_id)
+    via_socket = ViaSocket.get(driver, tunnel_id) # TODO: Check for errors
 
     case packet.get(:command_id)
     when CommandPacket::TUNNEL_DATA
-      socket.write(packet.get(:data))
+      puts("Received TUNNEL_DATA")
+      via_socket._sneak_in_data(packet.get(:data))
 
     when CommandPacket::TUNNEL_CLOSE
-      socket.close()
+      puts("Recieved TUNNEL_CLOSE")
+      via_socket.close()
     else
       raise(DnscatException, "Unknown command sent by the server: #{packet}")
     end
@@ -126,9 +135,50 @@ class ViaSocket
 end
 
 module DriverCommandTunnels
+  def _parse_host_ports(str)
+    local, remote = str.split(/ /)
+
+    if(remote.nil?)
+      @window.puts("Bad argument! Expected: 'listen [<lhost>:]<lport> <rhost>:<rport>'")
+      @window.puts()
+      raise(Trollop::HelpNeeded)
+    end
+
+    # Split the local port at the :, if there is one
+    if(local.include?(":"))
+      local_host, local_port = local.split(/:/)
+    else
+      local_host = '0.0.0.0'
+      local_port = local
+    end
+    local_port = local_port.to_i()
+
+    if(local_port <= 0 || local_port > 65535)
+      @window.puts("Bad argument! lport must be a valid port (between 0 and 65536)")
+      @window.puts()
+      raise(Trollop::HelpNeeded)
+    end
+
+    remote_host, remote_port = remote.split(/:/)
+    if(remote_host == '' || remote_port == '' || remote_port.nil?)
+      @window.puts("rhost or rport missing!")
+      @window.puts()
+      raise(Trollop::HelpNeeded)
+    end
+    remote_port = remote_port.to_i()
+
+    if(remote_port <= 0 || remote_port > 65535)
+      @window.puts("Bad argument! rport must be a valid port (between 0 and 65536)")
+      @window.puts()
+      raise(Trollop::HelpNeeded)
+    end
+
+    return local_host, local_port, remote_host, remote_port
+  end
+
   def _register_commands_tunnels()
-    @tunnels = []
     @sessions = {}
+    @via_sockets = {}
 
     @commander.register_command('wget',
       Trollop::Parser.new do
@@ -163,122 +213,86 @@ module DriverCommandTunnels
 #      end
 #    )
 
-#    @commander.register_command('listen',
-#      Trollop::Parser.new do
-#        banner("Listens on a local port and sends the connection out the other side (like ssh -L). Usage: listen [<lhost>:]<lport> <rhost>:<rport>")
-#      end,
-#
-#      Proc.new do |opts, optarg|
-#        local, remote = optarg.split(/ /)
-#
-#        if(remote.nil?)
-#          @window.puts("Bad argument! Expected: 'listen [<lhost>:]<lport> <rhost>:<rport>'")
-#          @window.puts()
-#          raise(Trollop::HelpNeeded)
-#        end
-#
-#        # Split the local port at the :, if there is one
-#        if(local.include?(":"))
-#          local_host, local_port = local.split(/:/)
-#        else
-#          local_host = '0.0.0.0'
-#          local_port = local
-#        end
-#        local_port = local_port.to_i()
-#
-#        if(local_port <= 0 || local_port > 65535)
-#          @window.puts("Bad argument! lport must be a valid port (between 0 and 65536)")
-#          @window.puts()
-#          raise(Trollop::HelpNeeded)
-#        end
-#
-#        remote_host, remote_port = remote.split(/:/)
-#        if(remote_host == '' || remote_port == '' || remote_port.nil?)
-#          @window.puts("rhost or rport missing!")
-#          @window.puts()
-#          raise(Trollop::HelpNeeded)
-#        end
-#        remote_port = remote_port.to_i()
-#
-#        if(remote_port <= 0 || remote_port > 65535)
-#          @window.puts("Bad argument! rport must be a valid port (between 0 and 65536)")
-#          @window.puts()
-#          raise(Trollop::HelpNeeded)
-#        end
-#
-#        @window.puts("Listening on #{local_host}:#{local_port}, sending connections to #{remote_host}:#{remote_port}")
-#
-#        begin
-#          @tunnels << Socketer.listen(local_host, local_port, {
-#            # This indent-level is the callbacks for the LOCAL socket (the
-#            # socket that the server is listening on)
-#            :on_connect => Proc.new() do |session, name|
-#              @window.puts("Connection from #{name}; forwarding to #{remote_host}:#{remote_port}...")
-#
-#              # These are the callbacks for the REMOTE socket (the fake socket that's on the remote side)
-#              create_via_socket('localhost', 4444, {
-#                :on_via => Proc.new() do |tunnel_id|
-#                  @sessions[session] = tunnel_id
-#                end,
-#                :on_ready => Proc.new() do |v_session, v_name|
-#                  @window.puts("[Tunnel #{@sessions[session]}] Tunnel ready: #{v_name}")
-#                end,
-#                :on_data => Proc.new() do |v_session, v_data|
-#                  @window.puts("[Tunnel #{@sessions[session]}] Data arrived from the other side! #{v_data.length} bytes")
-#                  session.write(v_data)
-#                end,
-#                :on_error => Proc.new() do |v_session, v_msg, v_e|
-#                  @window.puts("[Tunnel #{@sessions[session]}] Tunnel closed: #{v_msg} #{v_e}")
-#                end,
-#              })
-#
-#              session.ready!()
-#            end,
-#
-#            :on_data => Proc.new() do |session, data|
-#              tunnel_id = @sessions[session]
-#              if(tunnel_id.nil?)
-#                @window.puts("[Tunnel #{tunnel_id}] Unknown session!")
-#                next
-#              end
-#
-#              via_socket = @tunnels[tunnel_id]
-#              if(via_socket.nil?)
-#                @window.puts("[Tunnel #{tunnel_id}] Unknown tunnel!")
-#                next
-#              end
-#
-#              via_socket[:socket].write(data)
-#            end,
-#
-#            :on_error => Proc.new() do |session, msg, e|
-#              tunnel_id = @sessions[session]
-#              if(tunnel_id.nil?)
-#                @window.puts("[Tunnel #{tunnel_id}] Error in unknown session!")
-#                next
-#              end
-#
-#              via_socket = @tunnels[tunnel_id]
-#              if(via_socket.nil?)
-#                @window.puts("[Tunnel #{tunnel_id}] Error in unknown tunnel!")
-#                next
-#              end
-#
-#              # TODO: Kill the session properly
-#            end
-#          })
-#        rescue Errno::EACCES => e
-#          @window.puts("Sorry, couldn't listen on that port: #{e}")
-#        rescue Errno::EADDRINUSE => e
-#          @window.puts("Sorry, that address:port is already in use: #{e}")
-#          # TODO: Better error msg
-#        end
-#      end
-#    )
+    @commander.register_command('listen',
+      Trollop::Parser.new do
+        banner("Listens on a local port and sends the connection out the other side (like ssh -L). Usage: listen [<lhost>:]<lport> <rhost>:<rport>")
+      end,
+
+      Proc.new do |opts, optarg|
+        lhost, lport, rhost, rport = _parse_host_ports(optarg)
+        @window.puts("Listening on #{lhost}:#{lport}, sending connections to #{rhost}:#{rport}")
+
+        begin
+          # This listens on the server side, and creates a new ViaSocket for each connection
+          Socketer.listen(lhost, lport, {
+            # This indent-level is the callbacks for the SERVER socket (the
+            # socket that the local server is listening on)
+            :on_connect => Proc.new() do |session, name|
+              @window.puts("Connection from #{name}; forwarding to #{rhost}:#{rport}...")
+
+              # These are the callbacks for the REMOTE socket (the fake socket that's on the client side)
+              ViaSocket.new(self, rhost, rport, {
+                :on_via => Proc.new() do |v_session, via_socket|
+                  # Index the via_socket in such a way that the SERVER socketer instance can find it
+                  puts("Saving socket #{via_socket} under session #{session}")
+                  @via_sockets[session] = via_socket
+                end,
+                :on_ready => Proc.new() do |v_session, v_name|
+                  via_socket = @via_sockets[session]
+                  @window.puts("[Tunnel #{via_socket.tunnel_id}] Tunnel ready: #{v_name}")
+                  # Tell the SERVER session that we're ready to start receiving data
+                  session.ready!()
+                end,
+                :on_data => Proc.new() do |v_session, v_data|
+                  via_socket = @via_sockets[session]
+                  @window.puts("[Tunnel #{via_socket.tunnel_id}] Data arrived from the other side! #{v_data.length} bytes")
+                  # Write data from the REMOTE socket to the SERVER socket
+                  session.send(v_data)
+                end,
+                :on_error => Proc.new() do |v_session, v_msg, v_e|
+                  via_socket = @via_sockets[session]
+                  @window.puts("[Tunnel #{via_socket.tunnel_id}] Tunnel closed: #{v_msg} #{v_e}")
+                  v_session.close!()
+                  session.close!()
+                end,
+              })
+            end,
+
+            :on_data => Proc.new() do |session, data|
+              via_socket = @via_sockets[session]
+              if(via_socket.nil?)
+                @window.puts("[Tunnel #{tunnel_id}] Unknown session!")
+                next
+              end
+
+              via_socket.write(data)
+            end,
+
+            :on_error => Proc.new() do |session, msg, e|
+              puts("Looking up socket for session #{session}...")
+              via_socket = @via_sockets[session]
+              if(via_socket.nil?)
+                @window.puts("[Tunnel ???] Unknown session had an error: #{msg} #{e}")
+                next
+              end
+
+              via_socket.close()
+            end
+          })
+        rescue Errno::EACCES => e
+          @window.puts("Sorry, couldn't listen on that port: #{e}")
+        rescue Errno::EADDRINUSE => e
+          @window.puts("Sorry, that address:port is already in use: #{e}")
+          # TODO: Better error msg
+        rescue Exception => e
+          @window.puts("An exception occurred: #{e}")
+        end
+      end
+    )
   end
 
   def tunnel_data_incoming(packet)
-    ViaSocket.handle_packet(packet)
+    ViaSocket.handle_packet(self, packet)
   end
 
   def tunnels_stop()
