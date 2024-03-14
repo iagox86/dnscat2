a1088be7fd31b0360a8ab8bc605a19937e20aa8a
Pascal Ruffing <pruffing1@gmail.com>
2024-03-14 12:13:26 +0100

    Add support for Apple Silicon

--- client/libs/crypto/byte_order.h
+++ client/libs/crypto/byte_order.h
@@ -40,7 +40,7 @@ extern "C" {
 		__BYTE_ORDER == __LITTLE_ENDIAN) || \
 	defined(CPU_IA32) || defined(CPU_X64) || \
 	defined(__ia64) || defined(__ia64__) || defined(__alpha__) || defined(_M_ALPHA) || \
-	defined(vax) || defined(MIPSEL) || defined(_ARM_)
+	defined(vax) || defined(MIPSEL) || defined(_ARM_) || defined(__aarch64__)
 # define CPU_LITTLE_ENDIAN
 # define IS_BIG_ENDIAN 0
 # define IS_LITTLE_ENDIAN 1
